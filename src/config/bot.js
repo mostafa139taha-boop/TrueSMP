f871f148914fe49905454b36ff71c5e04edbd18c
mostafa139taha-boop <mostafa139taha@gmail.com>
2026-08-21 22:55:13 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Watching Over TrueSMP",     // this is what people actually see
+        state: "Watching Over Trunova Network",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
